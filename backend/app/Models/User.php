6be56c6aae09c5d0c2b18c63b03b6ded639b6359
Reyness <acollreynes@hotmail.com>
2025-02-04 19:07:43 +0100

    Salaries & Users db, model and relationship

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function salaries()
+    {
+        return $this->hasMany(Salary::class);
+    }
 }
